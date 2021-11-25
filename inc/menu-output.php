918bf678056107915e25d6460f19493a3fd0dad9
Steven Park <info@stevenpark.co.uk>
2021-11-25 23:09:30 +0000

    Menu output and font tweak

--- inc/menu-output.php
+++ inc/menu-output.php
@@ -1,4 +1,7 @@
 <?php 
+
+$output = "";
+
 // WP_Query arguments
 $args = array(
 	'post_type' => array( 'menu' ),
@@ -13,29 +16,29 @@ if ( $queryMenu->have_posts() ) {
 		$queryMenu->the_post();
 
     if (have_rows('section')):
-      echo '<nav aria-label="Jump to menus" class="mt-5 text-center text-uppercase"><p>Browse Menus</p>';
+      $output .= '<nav aria-label="Jump to menus" class="mt-5 text-center text-uppercase"><p>Browse Menus</p>';
       while(have_rows('section')) : the_row();
         $sectionname = get_sub_field('section_name');
         if ($sectionname) {
-          echo '<h3><a href="#section_'.preg_replace('/\W+/', '', $sectionname).'">'.$sectionname .'</a></h3>';
+          $output .= '<h3><a href="#section_'.preg_replace('/\W+/', '', $sectionname).'">'.$sectionname .'</a></h3>';
         }
       endwhile;
-      echo '</nav>';
+      $output .= '</nav>';
     endif;
   
-    echo "<div class='flex-wrap my-5'>";
+    $output .= "<div class='flex-wrap my-5'>";
     if (have_rows('section')):
       while(have_rows('section')) : the_row();
         $sectionname = get_sub_field('section_name');
         $sectionintro = get_sub_field('section_intro');
         if ($sectionname) {
-          echo "<div class='my-5' id='section_".preg_replace('/\W+/', '', $sectionname)."'>";
-          echo "<h3 class='text-center text-uppercase pb-4 mb-4 border-bottom'>".$sectionname ."</h3>";
+          $output .= "<div class='my-5' id='section_".preg_replace('/\W+/', '', $sectionname)."'>";
+          $output .= "<h3 class='text-center text-uppercase pb-4 mb-4 border-bottom'>".$sectionname ."</h3>";
         } else {
-          echo "<div class='my-5'>";
+          $output .= "<div class='my-5'>";
         }
         if ($sectionintro) {
-          echo "<p class='text-center border-bottom pb-4 text-uppercase'>". $sectionintro."</p>";
+          $output .= "<p class='text-center border-bottom pb-4 text-uppercase'>". $sectionintro."</p>";
         }
         if (have_rows("menu_item")):
           while(have_rows("menu_item")) : the_row();
@@ -45,42 +48,43 @@ if ( $queryMenu->have_posts() ) {
             $itemdiet = returnDietaryFormatted(get_sub_field('item_diet'));
             $hasoptions = get_sub_field('has_options');
   
-            echo "<div class='mb-2'>";
-            echo "<p class='d-flex flex-row flex-nowrap justify-content-between align-content-start mb-0'>";
-              echo "<span>";
-                echo "<strong>".$itemname ."</strong>". $itemdiet;
-                if ($itemdesc) {echo "<br />".$itemdesc;}
-              echo "</span>";
-              echo "<span class='d-flex pl-4 lead'><strong>".$itemprice."</strong></span>";
-            echo "</p>";
+            $output .= "<div class='mb-2'>";
+            $output .= "<p class='d-flex flex-row flex-nowrap justify-content-between align-content-start mb-0'>";
+              $output .= "<span>";
+                $output .= "<strong>".$itemname ."</strong>". $itemdiet;
+                if ($itemdesc) {$output .= "<br />".$itemdesc;}
+              $output .= "</span>";
+              $output .= "<span class='d-flex pl-4 lead'><strong>".$itemprice."</strong></span>";
+            $output .= "</p>";
             if ($hasoptions){
               if (have_rows("price_variation")):
                 while(have_rows("price_variation")) : the_row();
                   $vardesc = get_sub_field('variation_description');
                   $varprice = returnPriceFormatted(get_sub_field('variation_price'));
                   $vardiet = returnDietaryFormatted(get_sub_field('variation_dietary'));
-                  echo "<p class='d-flex flex-row flex-nowrap justify-content-between align-content-start ml-3 mb-0'>- ". $vardesc.$vardiet."<span class='pl-4 lead'><strong>".$varprice."</strong></span>". "</p>";
+                  $output .= "<p class='d-flex flex-row flex-nowrap justify-content-between align-content-start ml-3 mb-0'>- ". $vardesc.$vardiet."<span class='pl-4 lead'><strong>".$varprice."</strong></span>". "</p>";
                 endwhile;
               else: 
-                echo "Please add some price varations for this menu item";
+                $output .= "Please add some price varations for this menu item";
               endif;
             }
-            echo "</div>";
+            $output .= "</div>";
           endwhile;
-          echo "</div>";
+          $output .= "</div>";
         else:
-          echo "<blockquote>Please add some menu items to section '".$sectionname."'</blockquote>";
+          $output .= "<blockquote>Please add some menu items to section '".$sectionname."'</blockquote>";
         endif;
       endwhile;
   
     else:
-      echo "<blockquote>Please add some sections to this menu.</blockquote>";
+      $output .= "<blockquote>Please add some sections to this menu.</blockquote>";
     endif;
-    echo "</div>";
+    $output .= "</div>";
 	}
 }
 // Restore original Post Data
 wp_reset_postdata();
-				
+
+return $output;
 
 ?>
