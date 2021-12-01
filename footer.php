431bb0c3218d18a8cba493169b240cedadf4a640
Naomi Spirit <naomispirit@gmail.com>
2021-12-01 14:30:23 +0000

    Updates after chat with sabrina
    Mainly paddings on mobile etc.

---
+++ footer.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package Understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+$container = get_theme_mod( 'understrap_container_type' );
+?>
+
+<?php get_template_part( 'sidebar-templates/sidebar', 'footerfull' ); ?>
+
+<div class="wrapper" id="wrapper-footer">
+
+	<div class="<?php echo esc_attr( $container ); ?>">
+
+		<div class="row">
+
+			<div class="col-md-12">
+
+				<footer class="site-footer" id="colophon">
+
+					<div class="site-info d-flex justify-content-between">
+
+						<?php understrap_site_info(); ?>
+						<span>Green website hosting by <a href="Low carbon website developers">From This Day</a> and powered by <a href="http://my.kualo.com/uk/go/01089">Kualo</a></span>
+					</div><!-- .site-info -->
+
+				</footer><!-- #colophon -->
+
+			</div><!--col end -->
+
+		</div><!-- row end -->
+
+	</div><!-- container end -->
+
+</div><!-- wrapper end -->
+
+</div><!-- #page we need this extra closing tag here -->
+
+<?php wp_footer(); ?>
+
+</body>
+
+</html>
+
